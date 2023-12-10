c3c9ad0e535eeb525bb41213f2a5be0e9a4e503f
Geraldine <geraldine.castrocc@gmail.com>
2023-12-09 22:41:58 -0300

    qr png

--- script.js
+++ script.js
@@ -19,17 +19,16 @@ window.onload = function () {
 
         var webText = document.createElement("p");
         webText.classList.add("text-blue-400", "font-bold", "pb-8", "lg:pb-6", "text-center", "md:text-left", "fade-in");
-        webText.innerText = "Visita el sitio web:";
+        webText.innerText = "Descarga la App escaneando el QR:";
 
-        var webLink = document.createElement("a");
-        webLink.href = "https://merry-chebakia-e83e15.netlify.app/";
-        webLink.type = "button";
-        webLink.classList.add("slide-in-bottom-subtitle", "bounce-top-icons", "text-white", "bg-gradient-to-br", "from-purple-600", "to-blue-500", "hover:bg-gradient-to-bl", "focus:ring-4", "focus:outline-none", "focus:ring-blue-300", "dark:focus:ring-blue-800", "font-medium", "rounded-lg", "text-sm", "px-5", "py-2.5", "text-center", "me-2", "mb-2", "fade-in");
-        webLink.innerText = "RegistrAPP Web";
+        var webQrImage = document.createElement("img");
+        webQrImage.src = "qr-apk.png"; // Replace with the actual path to your QR code image
+        webQrImage.alt = "QR Code";
+        webQrImage.classList.add("max-w-full", "h-auto", "mx-auto", "fade-in", "w-1/2"); // Add styling as needed
 
         var linkContainer = document.createElement("div");
         linkContainer.classList.add("flex", "w-full", "justify-center", "md:justify-start", "pb-24", "lg:pb-0", "fade-in");
-        linkContainer.appendChild(webLink);
+        linkContainer.appendChild(webQrImage);
 
         container.appendChild(h1);
         container.appendChild(subtitle);
